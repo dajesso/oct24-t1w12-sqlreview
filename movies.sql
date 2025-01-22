6851fc4ff507dbcd9ea6b0e3bac10f9c6b3a4020
Jessica Amy <contact@jessicaamy.com>
2025-01-22 20:33:28 +1030

    Finished sql code

--- movies.sql
+++ movies.sql
@@ -20,5 +20,15 @@ create table movies(
     id serial primary key,
     title varchar(100) not null,
     length integer,
-    genre_id integer
-)
+    genre_id integer not null,
+    foreign key (genre_id) references genres(id) --on delete set null --on delete cascade
+);
+create table reviews(
+    id serial primary key,
+    stars integer  not null,
+    message text,
+    user_id integer not null,
+    movie_id integer not null,
+    foreign key (user_id) references users(id) on delete set null,
+    foreign key (movie_id) references movies(id) on delete cascade
+);
